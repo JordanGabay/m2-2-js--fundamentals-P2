b0d6e58db4cd4f8c3ad9f35135037e133d6df448
Jordan Gabay <gabay.jordan@outlook.com>
2020-06-16 18:09:33 -0400

    Exercise 1 Complete

--- workshop/exercise-1.js
+++ workshop/exercise-1.js
@@ -31,21 +31,53 @@ const notCode = [
 // Q1.1
 // How many names does Morty have? (use grandson)
 
+// const mortyNames = grandson.split(' ');
+// const numberOfNames = mortyNames.length;
+// console.log(`Morty has ${numberOfNames} names`);
+
+const numberOfNames = grandson.split(' ').length;
+console.log(`Morty has ${numberOfNames} names`);
+
 // Q1.2
 // Convert the alternateRicks string to an array. Console that array.
+
+// const alternateRicksArray = alternateRicks.split(', ')
+// console.log(alternateRicksArray);
+
+console.log('Answer 2:', alternateRicks.split(', '));
+
 
 // Q1.3
 // How many Ricks have been named in alternateRicks?
 
+// let count = 0;
+// alternateRicks.split(', ').forEach(function(currentelement){
+//   if(currentelement.includes('Rick'))
+//     count++; 
+// })
+// console.log('numberofricks: ', count);
+
+console.log('Answer 3:', alternateRicks.match(/Rick/g).length);
 // Q1.4
 // How many characters are there in rickSaying (without spaces)?
 
-// Q1.4
+console.log('Answer 4:', rickSaying.replace(/ /g, "").length);
+// Q1.5
 // Morty doesn't actually have a middle name. Console Morty's name without "Antoine".
 
-// Q1.5
+console.log('Answer 5:', grandson.replace("Antoine ", ""));
+
+// Q1.6
 // Decode and console. the secret!
 // To do so, you will need to remove all of the notCode words.
 
-// Q1.6 (Stretch Goal)
+let decodedMsg = secret;
+for (let x = 0; x < notCode.length; x++) {
+  decodedMsg = decodedMsg.replace (notCode[x], '')
+}
+console.log('Answer 6:', decodedMsg);
+
+// Q1.7 (Stretch Goal)
 // Scary Terry is actually not an enemy. Remove him from the list and console only Rick's true enemies.
+
+console.log('Answer 7:', enemies.replace("Scary Terry", ""));
