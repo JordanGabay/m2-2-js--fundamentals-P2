317f02e319039d4825cbfbc05cfb731e97eedcc6
Jordan Gabay <gabay.jordan@outlook.com>
2020-06-16 21:46:43 -0400

    Exercise 12 Complete

--- workshop/exercise-12.js
+++ workshop/exercise-12.js
@@ -10,6 +10,26 @@
 
 // Write function here
 
+function average(grades) {
+    let sum = 0;
+
+    for(let x=0; x < grades.length; x++){
+        sum +=grades[x];
+    }
+
+    let avg = Math.round(sum/grades.length);
+
+    if (avg < 60) return `F`;
+    if (avg >= 60 && avg < 70) return `D`;
+    if (avg >= 70 && avg < 80) return `C`;
+    if (avg >= 80 && avg < 90) return `B`;
+    if (avg >= 90 && avg < 100) return `A`;
+    
+}
+
 // Call the function with [48, 95, 65, 48, 59, 78, 72, 65]
 
+console.log(average([48, 95, 65, 48, 59, 78, 72, 65]));
 // Try with other values as well
+
+console.log(average([30, 90, 90, 92, 50, 45, 82, 79]));
